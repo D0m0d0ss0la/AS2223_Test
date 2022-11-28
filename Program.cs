f1011a9304545062aba5b535b643c82a55a6e1b3
D0m0d0ss0la <cristian.perilli@studenti.ittsrimini.edu.it>
2022-11-28 10:56:52 +0100

    fix: Traduzione in italiano

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, Mondo!");
+﻿tes1 test = new tes1();
+Console.WriteLine("Ciao, Mondo!, Io amo le divisioni! Eccone una: "); // tradotto
+Console.WriteLine(test.divisione());
+public class tes1
+{
+    int dividendo = 2;
+
+    public int divisore = 12;
+    public int divisione()
+    {
+        return (dividendo / divisore);
+    }
+}
